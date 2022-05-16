c6cd71634fe8a3bbc76090768dda313ec931846f
Jacory Armstrong <jacoryarmstrong@gmail.com>
2022-05-16 07:29:52 -0400

    added a new parallax page index updated vite project updated new images added gitignore

---
+++ dist/app.jsx
@@ -0,0 +1,69 @@
+import { useRef } from 'react';
+import moon from '<./img/hero.bg.png';
+import land from './img/illustraition-feature-tab-2.png';
+import cat from './cat.gif';
+
+import { Parallax, ParallaxLayer } from '@react-spring/parallax';
+
+function App() {
+  const ref = useRef();
+
+  return (
+    <div>
+      <Parallax pages={4} ref={ref}>
+        {/* <ParallaxLayer speed={1}>
+            <h2>Welcome to my website</h2>
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={1} speed={0.5}>
+            <h2>Web development is fun!</h2>
+        </ParallaxLayer> */}
+
+        <ParallaxLayer
+          offset={0}
+          speed={1}
+          factor={2}
+          style={{
+            backgroundImage: `url(${Image})`,
+            backgroundSize: 'cover',
+          }}
+        />
+
+        <ParallaxLayer
+          offset={2}
+          speed={1}
+          factor={4}
+          style={{
+            backgroundImage: `url(${land})`,
+            backgroundSize: 'cover',
+          }}
+        ></ParallaxLayer>
+
+        <ParallaxLayer
+          sticky={{ start: 0.9, end: 2.5 }}
+          style={{ textAlign: 'center' }}
+        >
+          <img src={cat} />
+        </ParallaxLayer>
+
+        <ParallaxLayer
+          offset={0.2}
+          speed={0.05}
+          onClick={() => ref.current.scrollTo(3)}
+        >
+          <h2>Welcome to my website</h2>
+        </ParallaxLayer>
+
+        <ParallaxLayer
+          offset={3}
+          speed={2}
+          onClick={() => ref.current.scrollTo(0)}
+        >
+          <h2>Hi Mom!</h2>
+        </ParallaxLayer>
+      </Parallax>
+    </div>
+  );
+}
+
+export default App;
